b9fc6a2889cbe9dd53b5b7baac0557f7c99d33cf
Aluno São Lucas <aluno.saolucas@ensino.ulbra.net>
2025-12-09 18:50:00 -0500

    subindo alterações 0.1

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -13,3 +13,28 @@ function login() {
     // Entra na home
     window.location.href = "home.html";
 }
+document.addEventListener("DOMContentLoaded", function () {
+
+    const form = document.getElementById("loginForm");
+
+    form.addEventListener("submit", function (event) {
+        event.preventDefault(); // Evita o formulário recarregar a página
+
+        const usuario = document.getElementById("username").value;
+        const senha = document.getElementById("password").value;
+
+        // Usuário correto
+        const usuarioCorreto = "claudio";
+        const senhaCorreta = "1234";
+
+        if (usuario === usuarioCorreto && senha === senhaCorreta) {
+            alert("Login realizado com sucesso!");
+
+            // Redireciona para a página inicial
+            window.location.href = "inicial.html";
+        } else {
+            alert("Usuário ou senha incorretos. Tente novamente.");
+        }
+    });
+});
+
